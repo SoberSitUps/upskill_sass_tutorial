8129d5cc3460e339eba693751c2780a1a93d80c2
Stephen Walter <stephenjwalter22@gmail.com>
2017-07-13 19:43:29 +0000

    added flash notice for invalid plan id

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,18 +1,27 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-    #Extend default Devise gem behavior. Pro users (plan id-2) are saved with 
-    #Stripe subscription function
-    #Otherwise use normal Devise signup
-    def create
-      super do |resource|
-        if params[:plan]
-          resource.plan_id = params[:plan]
-          if resource.plan_id == 2
-            resource.save_with_subscription
-          else
-            resource.save
-          end
+  before_action :select_plan, only: :new
+  #Extend default Devise gem behavior. Pro users (plan id-2) are saved with 
+  #Stripe subscription function
+  #Otherwise use normal Devise signup
+  def create
+    super do |resource|
+      if params[:plan]
+        resource.plan_id = params[:plan]
+        if resource.plan_id == 2
+          resource.save_with_subscription
+        else
+          resource.save
         end
       end
-      
     end
+  end
+  
+  private
+    def select_plan
+      unless (params[:plan] == '1' || params[:plan] == '2')
+      flash[:notice] = "Select plan tyle to sign up"
+        redirect_to root_url
+      end
+    end
+  
 end
